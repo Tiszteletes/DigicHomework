ae7f47227e1bfc0610abadab9f5e8c5aff7f6dfd
Tiszteletes <daniel.papp.87@gmail.com>
2025-09-29 19:14:08 +0200

    feat: Initial commit

---
+++ DigicHomework.Domain/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DigicHomework.Domain
+{
+    public class Employee
+    {
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("jobTitleName")]
+        public string JobTitleName { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("preferredFullName")]
+        public string PreferredFullName { get; set; }
+
+        [JsonPropertyName("employeeCode")]
+        public string EmployeeCode { get; set; }
+
+        [JsonPropertyName("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonPropertyName("emailAddress")]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ DigicHomework.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DigicHomework.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
